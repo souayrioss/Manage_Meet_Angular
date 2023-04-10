7acbed840ff6edd6dc230c30116b9dbd842ad91c
Oussama Souayri <souayri.oussama@gmail.com>
2023-04-10 22:27:36 +0000

    some updates login c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,25 @@ import { AppRoutingModule } from './app-routing.module';
 import {IndexComponent} from "./front_office/index/index.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import { NewMeetComponent } from './back_office/new-meet/new-meet.component';
+import { MeetsComponent } from './back_office/meets/meets.component';
+import { NewTicketComponent } from './support/new-ticket/new-ticket.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     IndexComponent,
-    NewMeetComponent
+    NewMeetComponent,
+    MeetsComponent,
+    NewTicketComponent
   ],
   imports: [
     BrowserModule,
     RouterOutlet,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
